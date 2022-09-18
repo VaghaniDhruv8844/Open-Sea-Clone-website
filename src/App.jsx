ebf235a9648f35907ff2c1f40be77f8042f6e3a2
VaghaniDhruv8844 <vaghani.dhruv41@gmail.com>
2022-09-18 15:04:28 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,165 @@
+import { useEffect, useState } from "react";
+import { useMoralis } from "react-moralis";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink,
+  Redirect,
+} from "react-router-dom";
+import Account from "components/Account";
+import Chains from "components/Chains";
+import NFTBalance from "components/NFTBalance";
+import NFTTokenIds from "components/NFTTokenIds";
+import { Menu, Layout } from "antd";
+import SearchCollections from "components/SearchCollections";
+import "antd/dist/antd.css";
+import NativeBalance from "components/NativeBalance";
+import "./style.css";
+import Text from "antd/lib/typography/Text";
+import NFTMarketTransactions from "components/NFTMarketTransactions";
+const { Header, Footer } = Layout;
+
+const styles = {
+  content: {
+    display: "flex",
+    justifyContent: "center",
+    fontFamily: "Roboto, sans-serif",
+    color: "#041836",
+    marginTop: "130px",
+    padding: "10px",
+  },
+  header: {
+    position: "fixed",
+    zIndex: 1,
+    width: "100%",
+    background: "#fff",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    fontFamily: "Roboto, sans-serif",
+    borderBottom: "2px solid rgba(0, 0, 0, 0.06)",
+    padding: "0 10px",
+    boxShadow: "0 1px 10px rgb(151 164 175 / 10%)",
+  },
+  headerRight: {
+    display: "flex",
+    gap: "20px",
+    alignItems: "center",
+    fontSize: "15px",
+    fontWeight: "600",
+  },
+};
+const App = ({ isServerInfo }) => {
+  const { isWeb3Enabled, enableWeb3, isAuthenticated, isWeb3EnableLoading } =
+    useMoralis();
+
+  const [inputValue, setInputValue] = useState("explore");
+
+  useEffect(() => {
+    if (isAuthenticated && !isWeb3Enabled && !isWeb3EnableLoading) enableWeb3();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isAuthenticated, isWeb3Enabled]);
+
+  return (
+    <Layout style={{ height: "100vh", overflow: "auto" }}>
+      <Router>
+        <Header style={styles.header}>
+          <Logo />
+          <SearchCollections setInputValue={setInputValue} />
+          <Menu
+            theme="light"
+            mode="horizontal"
+            style={{
+              display: "flex",
+              fontSize: "17px",
+              fontWeight: "500",
+              marginLeft: "50px",
+              width: "100%",
+            }}
+            defaultSelectedKeys={["nftMarket"]}
+          >
+            <Menu.Item key="nftMarket" onClick={() => setInputValue("explore")}>
+              <NavLink to="/NFTMarketPlace">🛒 Explore Market</NavLink>
+            </Menu.Item>
+            <Menu.Item key="nft">
+              <NavLink to="/nftBalance">🖼 Your Collection</NavLink>
+            </Menu.Item>
+            <Menu.Item key="transactions">
+              <NavLink to="/Transactions">📑 Your Transactions</NavLink>
+            </Menu.Item>
+          </Menu>
+          <div style={styles.headerRight}>
+            <Chains />
+            <NativeBalance />
+            <Account />
+          </div>
+        </Header>
+        <div style={styles.content}>
+          <Switch>
+            <Route path="/nftBalance">
+              <NFTBalance />
+            </Route>
+            <Route path="/NFTMarketPlace">
+              <NFTTokenIds
+                inputValue={inputValue}
+                setInputValue={setInputValue}
+              />
+            </Route>
+            <Route path="/Transactions">
+              <NFTMarketTransactions />
+            </Route>
+          </Switch>
+          <Redirect to="/NFTMarketPlace" />
+        </div>
+      </Router>
+      <Footer style={{ textAlign: "center" }}>
+        <Text style={{ display: "block" }}>
+          ⭐️
+          <a
+            href="https://github.com/ethereum-boilerplate/ethereum-boilerplate/"
+            target="_blank"
+            rel="noopener noreferrer"
+          ></a>
+          , every star makes us very happy!
+        </Text>
+
+        <Text style={{ display: "block" }}>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://forum.moralis.io/t/ethereum-boilerplate-questions/3951/29"
+          ></a>
+        </Text>
+
+        <Text style={{ display: "block" }}>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://moralis.io?utm_source=boilerplatehosted&utm_medium=todo&utm_campaign=ethereum-boilerplat"
+          ></a>
+        </Text>
+      </Footer>
+    </Layout>
+  );
+};
+
+export const Logo = () => (
+  <div style={{ display: "flex" }}>
+    <svg
+      width="60"
+      height="38"
+
+      // viewBox="0 0 50 38"
+      // fill="none"
+      // xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M43.6871 32.3986C43.5973 32.4884 43.53 32.5782 43.4402 32.6905C43.53 32.6007 43.5973 32.5109 43.6871 32.3986Z"
+        fill="black"
+      />
+    </svg>
+  </div>
+);
+
+export default App;
